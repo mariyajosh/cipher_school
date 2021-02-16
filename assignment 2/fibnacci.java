dedf10ed724224f078571919e321a15f540d7368
mariya josh <mariyajosh2018@gmail.com>
2021-02-16 14:24:35 +0530

    assignment 2

---
+++ assignment 2/fibnacci.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class fibnacci{
+static int fib(int n)
+{
+if(n==1||n==2)
+{
+    return 1;
+}
+else{
+return(fib(n-1)+fib(n-2));
+}
+}
+    public  static void main(String ar[]) {
+
+        System.out.println(fib(10));
+        
+    }
+}
